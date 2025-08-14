cf9ed3f1f7b60b58e4ccdceebc86a38b10193a6f
Veronika-eltex <veronika-eltex@gmail.com>
2025-08-14 17:13:27 +0700

    changes in BGP connection verification

--- test_ME/tests/test_ME.py
+++ test_ME/tests/test_ME.py
@@ -9,6 +9,10 @@ test_hosts = ['r2-g3', 'r1-g3', 'r4-g3']
 start_ip = ipaddress.IPv4Address("192.168.168.41")
 end_ip = ipaddress.IPv4Address("192.168.168.43")
 
+#генерируем ожидаемые ip из диапазона
+expected_ips = {str(ipaddress.IPv4Address(ip_int)) 
+               for ip_int in range(int(start_ip), int(end_ip) + 1)}
+
 def is_ip_in_range(ip_with_mask, start_ip, end_ip):
     ip_str = ip_with_mask.split("/")[0]
     
@@ -22,14 +26,23 @@ def is_ip_in_range(ip_with_mask, start_ip, end_ip):
 def test_vrf_check(host):
     result = execute_vrf_check(host)
     
-    # Проверяем статус выполнения
     assert result["status"], f"Ошибка на хосте {host}: {result['error']}"
     
-    # Проверяем парсированные данные
+    #создаем массив полученных ip
+    ips_in_range = []
+    
+    #проверяем наличие всех ip в полученных данных
     for i, row in enumerate(result["data"]):
-        if len(row) > 2:  # Убедимся, что есть второй элемент
+        if len(row) > 2:  #
             ip_str = row[2]
-            if not is_ip_in_range(ip_str, start_ip, end_ip):
+            if is_ip_in_range(ip_str, start_ip, end_ip):
+                ips_in_range.append(ip_str.split("/")[0])
+            else:
                 pytest.fail(
-                    f"Хост {host}, строка {i}: IP '{ip_str}' не в диапазоне 192.168.168.41–192.168.168.43"
+                    f"Хост {host}, строка {i}: IP '{ip_str}' не в диапазоне {start_ip}–{end_ip}"
                 )
+    
+    #вывод потерянного ip
+    found_ips = set(ips_in_range)
+    missing_ips = expected_ips - found_ips
+    assert not missing_ips, f"Хост {host}: отсутствуют IP-адреса {missing_ips}"
